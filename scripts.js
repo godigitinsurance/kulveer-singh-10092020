165f1614a86f0b1c65a94329e81e812d0cf4afcd
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-09-09 11:18:40 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Kulveer Singh");
+$("#policyNumber").text("DL8446090");
+$("#startDate").text("10-Sep-2020");
+$("#endDate").text("09-Sep-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08CZ5513");
+$("#make").text("TOYOTA");
+$("#model").text("ETIOS");
+$("#vehicalSubType").text("GD (NP)");
+$("#engineNo").text("1ND1405690");
+$("#chassis").text("MBJB494BTX003200315");
